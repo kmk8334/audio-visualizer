f34009d638f0501afe28941ee7ea042a586bc356
Kevin K <kmk8334@kgcoe-git.rit.edu>
2020-03-23 23:01:06 -0400

    Added mobile functionality, fixed fullscreen color

--- src/utils.js
+++ src/utils.js
@@ -6,17 +6,6 @@ Purpose: Provide a helper function library for commonly called functions
 const makeColor = (red, green, blue, alpha = 1) => {
     return `rgba(${red},${green},${blue},${alpha})`;
 };
-  
-  /*const getRandom = (min, max) => {
-    return Math.random() * (max - min) + min;
-  };*/
-  
-  /*const getRandomColor = () => {
-      const floor = 35; // so that colors are not too bright or too dark 
-    const getByte = () => getRandom(floor,255-floor);
-    return `rgba(${getByte()},${getByte()},${getByte()},1)`;
-  };*/
-
 
 // Create a linear gradient out of the given color stops
 const getLinearGradient = (ctx,startX,startY,endX,endY,colorStops) => {
